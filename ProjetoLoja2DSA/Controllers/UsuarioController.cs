71160e2845939f381723f6a5ad467d0cc57f12f7
caio <caiods786@gmail.com>
2025-10-01 17:39:04 -0300

    5-Criando a lógica do UsuarioController

--- ProjetoLoja2DSA/Controllers/UsuarioController.cs
+++ ProjetoLoja2DSA/Controllers/UsuarioController.cs
@@ -1,9 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
+﻿//IMPORTA AS BIBLIOTECAS QUE SERÃO UTILIZADAS NO PROJETO
+using Microsoft.AspNetCore.Mvc;
+using ProjetoLoja2DSA.Controllers;
+using ProjetoLoja2DSA.Repositorio;
+//DEFINE O NOME E ONDE A CLASSE USUARIOCONTROLLER ESTÁ LOCALIZADA
+//NAMESPACE AJUDA A ORGANIZAR O CÓDIGO E EVITAR CONFLITOS.
 namespace ProjetoLoja2DSA.Controllers
 {
+    //CLASSE USUARIOCONTROLLER QUE ESTÁ HERDANDO DA CLASSE PAI CONTROLLER
     public class UsuarioController : Controller
     {
+        //DECLARA A VARIÁVEL PRIVADA E SOMENTE LEITURA DO TIPO USUARIOREPOSITORIO
+        //INSTANCIA O USUARIOCONTROLLER PARA SER ATRIBUÍDO NO CONSTRUTOR E INTERAJIR COM OS DADOS
+        private readonly UsuarioRepositorio _usuarioRepositorio;
+
+        //DEFINE O CONSTRUTOR DA CLASSE USUARIOCONTROLLER
+        //RECEBE A INSTANCIA DE USUARIOREPOSITORIO COM PARAMETROS
+        public UsuarioController(UsuarioRepositorio usuarioRepositorio)
+        {
+             //O CONSTRUTOR É CHAMADO QUANDO UMA NOVA INSTANCIA É CRIADA
+            _usuarioRepositorio = usuarioRepositorio;
+        }
+        
+        
+        
+        //INTERFACE QUE REPRESENTA O RESULTADO DA PÁGINA
         public IActionResult Index()
         {
             return View();
